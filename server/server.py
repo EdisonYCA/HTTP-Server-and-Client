d7ed05ab750abc44182da788d9f7b123d882661a
Edison Caceres <edisonyca127@gmail.com>
2024-10-06 12:10:57 -0400

    server can now handle "/index" get request.

--- server/server.py
+++ server/server.py
@@ -10,42 +10,62 @@ class Server:
         self.host = socket.gethostname()
         self.default_file = "server/pages/index.html"
 
+
     def init_server(self):
-        """ function to initalize the welcoming socket """
+        """  initalize the welcoming socket, exit program if any issue doing so. """
         try:
             self.server_socket.bind((self.host, self.port))
         except socket.error as e:
+            print(f"an issue occurred binding the welcoming socket: {e}")
             sys.exit()
+        self.server_socket.listen(1) # accept at most 1 client
 
-        self.server_socket.listen(1)
 
-    def accept_clients(self):
-        print("awaiting connections...")
+    def serve_file(self, path):
+        """ fetch file at path, return 404 status code if file does not exist. """
+        path = self.construct_path(path) # "/" will automatically fetch index.html
+        response = ""
+        try:
+            with open(path, "r", encoding="utf-8") as f:
+                response = f"HTTP/1.1 200 OK\n\n {f.read()}"
+        except FileNotFoundError:
+            response = "HTTP/1.1 404 File Not Found"
+        return response
+
+
+    def construct_path(self, path):
+        """ take the path request of client and construct it """
+        if path == "/" or path == "/index":
+            return self.default_file
+
+
+    def handle_clients(self):
+        """ accept clients and send back server responses """
+        print("server awaiting a client...")
         connection_socket, addr = self.server_socket.accept()
         request = connection_socket.recv(1024)
-        contents = self.handle_request(request)
-        connection_socket.send(contents.encode())
+        server_response = self.handle_request(request)
+        connection_socket.send(server_response.encode())
         connection_socket.close()
-        return request
+        print("client handled")
 
 
     def handle_request(self, request):
-        request = request.split()
-        method = request[0].decode("utf-8")
-        path = request[1].decode("utf-8")
+        """ handle a clients request """
+        request = request.decode("utf-8") # convert HTTP request from byte-stream to utf-8 str
+        request = request.split() # delimit the request by whitespace
+        method = request[0]
+        path = request[1]
 
-        print(method, path)
         if method == "GET":
-            if path == "/":
-                file_contents = open(self.default_file, "r").read()
-                print(file_contents)
-                response = f"HTTP/1.1 200 OK\n\n{file_contents}"
-                return response
-            
+            return self.serve_file(path)
+
+
+           
 if __name__ == "__main__":
     # set-up welcoming socket
     server = Server()
     server.init_server()
 
-    # accept incoming connections
+    # handle incoming connections
     server.handle_clients()
