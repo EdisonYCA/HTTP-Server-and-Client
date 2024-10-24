0c593b76ec87f5b2d056fed425c40470f7596a2b
Edison Caceres <edisonyca127@gmail.com>
2024-10-24 19:30:40 -0400

    Added code to send HTTP requests, handle HTTP response content, and removed port input

--- client/client.py
+++ client/client.py
@@ -1,5 +1,6 @@
 import re
 import socket
+import sys
 
 class Client:
     """ class representing a client """
@@ -12,20 +13,41 @@ class Client:
         # get uri from user
         uri = self.get_uri()
 
-        # attempt connection with the server
+        # handle user quit
+        if uri == "q":
+            sys.exit()
+
+        # destination IP and port
         server_ip = uri[7:16]
         port = int(uri[17:21])
 
+        # assembling HTTP request
+        file = uri[21:]
+        http_req = ["GET", file]
+
         try:
+            # attempt connection with server
             self.client_socket.connect((server_ip, port))
             print("connection established!")
+
+            # connection established, send HTTP request
+            self.client_socket.send(' '.join(http_req).encode())
+
+            # retrieve server response
+            file_contents = self.client_socket.recv(2048)
+            file_contents = file_contents.decode("utf-8") # byte strea mto utf-8
+
+            # display the file contents (using utf-8)
+            print(f"file contents: \n{file_contents}")
+
+            # close connection
             self.client_socket.close()
         except socket.error as e:
             print(f"an error occured connecting to the server: {e}")
 
     def get_uri(self):
         """ gets the client input and returns it's validated form """
-        request = "Enter a valid URI in the form of http://server ip/page or enter q to quit: "
+        request = "Enter a valid URI in the form of http://server_ip:8080/page or enter q to quit: "
         uri = input(request)
         while uri != "q" and not self.validate_uri(uri):
             uri = input(request)
@@ -34,9 +56,8 @@ class Client:
     def validate_uri(self, uri):
         """ validates the using regex """
         ip = r"\d{3}(.\d){3}" # nnn.n.n.n
-        port = r"\d{4}" # port will always be 8080 (as of now)
-        file = r"([A-Za-z].[A-Za-z])*"
-        pattern = rf"http://{ip}:{port}/{file}"
+        file = r"([A-Za-z]/?)*"
+        pattern = rf"http://{ip}:8080/{file}"
 
         if re.fullmatch(pattern, uri):
             return True
